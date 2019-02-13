e8872e099e08ea759851e95446646b06c42ba1a1
Om <omsirvi.prakash@gmail.com>
2019-02-13 21:00:26 +0530

    for loop using sqr without function and with function

---
+++ forloop.py
@@ -0,0 +1,19 @@
+# for loop using print squre value
+
+a = [1,2,3,4,5]
+for i in a:
+    sqr = i*i
+    print(sqr)
+
+
+#using function
+
+a = [1,2,3,4,5]
+
+def sqr(a):
+    return sqr
+
+for i in a:
+    sqr = i*i
+    print(sqr)
+
