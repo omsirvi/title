8d73610e37dabda32e769209daf581b77a2cb197
Om <omsirvi.prakash@gmail.com>
2019-02-13 23:10:29 +0530

    pass and continue program

---
+++ pass.py
@@ -0,0 +1,24 @@
+# pass and cont program
+
+st = 'kamlesh'
+
+for st in st:
+    print(st)
+
+    if st == 'l':
+        pass
+        print("enter value")
+
+
+
+
+# cont program
+
+st1 = 'prakash'
+for st1 in st1:
+    print(st1)
+
+    if st1 == 'k':
+        continue
+        print("enter value")
+
